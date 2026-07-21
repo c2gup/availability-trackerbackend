e1fadd1798355fa49c4fcb9f47555f2719be3016
Anupam gupta <c2gupt@gmail.com>
2026-07-21 15:31:23 +0530

    Update allowed origins for CORS to include the correct beta frontend URL

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const allowedOrigins = [
   "https://availabilitytrackerfrontend.vercel.app",
   "http://localhost:3000",
   "http://localhost:5174",
-  "https://availability-trackerfrontend-beta.vercel.app",
+  "https://availability-trackerfrontend-iota.vercel.app/",
 ];
 
 app.use(
